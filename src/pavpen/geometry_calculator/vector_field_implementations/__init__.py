32924c0517a28102b6ffa1a33434f04f541dab9c
Pavel M. Penev <pavpen@gmail.com>
2026-04-20 18:30:21 -0400

    feat: Re-export symbols for shorter name paths
    * Re-export classes, constants, and other symbols from parent packages,
      so that they're accessible from a shorter namespace path.
    * Export `__version__`, `__version_tuple__`, and `__commit_id__` from
      the top-level package.  (Otherwise, they're package-private.)
    
    changelog: ignore

---
+++ src/pavpen/geometry_calculator/vector_field_implementations/__init__.py
@@ -0,0 +1,6 @@
+from pavpen.geometry_calculator.vector_field_implementations.complex_vector_field_operations import (
+    ComplexVectorFieldOperations as ComplexVectorFieldOperations,
+)
+from pavpen.geometry_calculator.vector_field_implementations.tuple_vector_field_float_operations import (
+    TupleVectorFieldFloatOperations as TupleVectorFieldFloatOperations,
+)
